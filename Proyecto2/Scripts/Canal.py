0972be707a6b4321cd4f254a2bcb7d282d83ea2f
Esteicy <esteicy15@gmail.com>
2023-10-31 15:16:57 -0600

    Agregar desfase en grados al eco

--- Proyecto2/Scripts/Canal.py
+++ Proyecto2/Scripts/Canal.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def canal(signal, num_puntos, t):
+def canal(signal, num_puntos, t, fs):
     entrada = int(input("1.Ruido\n2.Eco\n\nOpcion: "))
     if(entrada == 1):
         amplitud_ruido = float(input("\nNivel de ruido: "))
@@ -14,10 +14,13 @@ def canal(signal, num_puntos, t):
         atraso = 0
         senal_con_eco = np.copy(signal)
         for amp in range(cantidadEco):
-            atraso += 0.2
+            print("\n")
+            grados_retraso = float(input("Desfase en grados de eco "+str(amp+1)+": "))
+            radianes_retraso = np.deg2rad(grados_retraso)  # Convierte grados a radianes
+            atraso = (radianes_retraso * num_puntos) / (2 * np.pi * fs)
             amplitud = float(input("Amplitud de eco "+str(amp+1)+": "))
-            eco = np.roll(signal, int(atraso * num_puntos)) * amplitud
-            eco[:int(atraso * num_puntos)] = 0
+            eco = np.roll(signal, int(atraso * fs)) * amplitud
+            eco[:int(atraso * fs)] = 0
             ecos.append(eco)
             senal_con_eco += eco
             plt.plot(t,eco, label="Eco "+str(amp+1))
@@ -38,11 +41,11 @@ phi = 0        # Fase
 num_puntos = 1000
 # Tiempo
 t = np.linspace(0, 2, num_puntos)  # Generar valores de tiempo de 0 a 2 segundos
-
+fs = 1.0 / (t[1] - t[0]) 
 # Generar la señal de coseno elevado
 cosine_signal = A * np.cos(2 * np.pi * f * t + phi)**n
 
-noised_signal= canal(cosine_signal, num_puntos, t)
+noised_signal= canal(cosine_signal, num_puntos, t, fs)
 
 plt.subplot(2, 1, 1)
 plt.plot(t,cosine_signal)
